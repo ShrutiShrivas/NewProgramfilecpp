7111f7064ce1d0e20c508e2dd7699ad7938fbca0
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-09-23 18:14:38 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout<<"enter the number";
+	cin>>num;
+	
+	if(num<=0)
+	{
+		cout<<"Number is neagative";
+	}
+	else
+	{
+		cout<<"Number is positive";
+	}
+}
+
