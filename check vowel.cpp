46b74d771e8b94700e3de674c8e44ca095b3188a
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-09-23 18:21:07 +0530

    Add files via upload

---
+++ check vowel.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char  ch;
+	
+	cout<<"enter the char";
+	cin>>ch;
+	
+	if(ch=='a'|| ch=='e' || ch=='i' || ch=='o' || ch=='u')
+	{
+		cout<<"vowels";
+	}
+	else
+	{
+		cout<<"consonatant";
+	}
+	
+}
