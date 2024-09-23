8d4294f51d56e612a9093c848ab818a8bfc83706
ShrutiShrivas <101803281+ShrutiShrivas@users.noreply.github.com>
2024-09-23 18:14:05 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	
+cout<<"enter a value of a";
+cin>>a;
+cout<<"enter b value of b";
+cin>>b;
+	
+	  c=a+b;
+	  
+	 cout<<c;
+	 
+	 return 0;
+}
